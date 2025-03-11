1e4d85fe876e65bc88e61a4ac8c4bd89f3b7329e
donthumadhui <donthumadhuri1@gmail.com>
2025-03-11 11:01:38 +0530

    project

---
+++ feedback.js
@@ -0,0 +1,29 @@
+// document.getElementById('submit-btn').addEventListener('click', function() {
+    
+//     const name = document.getElementById('name').value;
+//     const email = document.getElementById('email').value;
+//     const branch = document.getElementById('branch').value;
+//     const role = document.getElementById('role').value;
+//     const year = document.getElementById('year').value;
+//     const review1 = document.getElementById('review1').value;
+//     const review2 = document.getElementById('review2').value;
+//     const review3 = document.getElementById('review3').value;
+//     const review4 = document.getElementById('review4').value;
+
+    
+//     if (!name || !email || !branch || !role || !year || !review1 || !review2 || !review3 || !review4) {
+        
+//         alert('Please fill in all required fields.');
+//     } else {
+        
+//         document.querySelector('.container').style.display = 'none'; 
+        
+        
+//         const responseMessage = document.getElementById('response-message');
+//         responseMessage.classList.remove('hidden');
+//         responseMessage.innerHTML = `<p>Thank you for your feedback, ${name}!</p>`;
+//     }
+// });
+
+
+
